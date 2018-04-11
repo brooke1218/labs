30b9dd3d4f8a9365735b2d15ff1689c9f3deee94
Brooke <brooke1218@gmail.com>
2018-04-11 15:38:32 -0400

    lab8

--- lab8.py
+++ lab8.py
@@ -27,9 +27,34 @@ def count_lines(fname):
     """
     fin = open(fname, 'r') # create file object fin for reading
     # define and initialize local variable num_lines to store result
+    num_line = []
 
     for line in fin: #iterate through each line in fin
         # increment num_lines
+        num_line = num_line+1
+        pass
     fin.close()
     #modify statement below to return reduce result
     return 0
+
+def count_word(word, fname):
+    """
+    Counts the number of times word appears in fname input text file
+    word: string - desired word to count
+    fname: string file name
+    Returns: integer -number of times word appears in the input text file
+    """
+
+    #create file object fin for reading
+    fin = open(fname, 'r')
+    # create local variable nu,_words in which we count the occurrneces of word in the file
+    num_words = 0
+
+    for line in fin:
+        list_of_words = line.split()
+
+        if word in list_of_words:
+            num_words= num_words +1 
+
+    fin.close()
+    return num_words
