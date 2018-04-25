bc3b034f45781dacc8aa59b3e35141fc74be2657
Brooke <brooke1218@gmail.com>
2018-04-25 14:57:45 -0400

    lab10

--- lab10.py
+++ lab10.py
@@ -41,7 +41,7 @@ def count_at_start(word, source):
     for line in fin:
         list_of_words = line.split()
         if list_of_words:
-            if word ==list_of_words(0):
+            if word ==list_of_words[0]:
                 count = count + 1
     fin.close()
     return count
@@ -57,9 +57,55 @@ def split_fname(source):
     puts a ',' between words in the file
     """
 
-    file = with open(“testfile.txt”,”w”)
-    file.write(“Hello World”)
-    
+    file = open('testfile.txt','w')
+    file.write('Hello World')
+
     x = source.split(',')
     for word in source:
-        print word
+        print(word)
+
+def count_at_end(word, source):
+    """
+    counts lines with word at the
+    end of each line in the file
+    source.
+    word- str
+    source: string - name of the file"""
+    count = 0
+    fin = open(source)
+    for line in fin:
+        list_of_words = line.split()
+        if list_of_words:
+            if list_of_words[-1] == word:
+                count += 1
+    return count
+
+def letter_count(source, letter_list):
+    """counts how many times letters from
+    letter_list appear in file source
+    source: stri -name of file
+    letter_list: list of characters"""
+
+
+
+def remove_word_from_file(word, source,target):
+    """counts occurences of word at the start
+    of the line and remove word from the file
+    word: string - to be counted and removed
+    source: string - name of input file
+    target: string - name of output afile
+    returns: int - number of removed words"""
+
+    count = 0
+    fin = open(source)
+    fout = open(target, 'w')
+    for line in fin:
+        if line.startswith(word):
+            count += 1
+            new_line = line.replace(word, '')
+        else:
+            fout.write(new_line)
+
+    fin.close()
+    fout.close()
+    return count
